24805d10efd13395cf1db01785b6667d56bb80ca
papanpapan <samim.skmolla@gmail.com>
2017-03-15 18:25:25 +0530

    ASCII
    ASCII

---
+++ src/ASCII.java
@@ -0,0 +1,19 @@
+
+public class ASCII {
+
+	
+	public static void main(String[] args) {
+		int ch=0;
+		char i=0;
+		for (i=65; i<=90; i++)
+		
+		{
+			ch=(char)i;
+		     System.out.println("\n"+ch+">=====>"+i);
+		
+		
+		}
+
+	}
+
+}
